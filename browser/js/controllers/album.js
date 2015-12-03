6d8126081414795986dd7b2ac50d12f26ce66dd4
jonatans <jonatan@jschumacher.com>
2015-12-03 17:56:08 -0500

    much better

--- browser/js/controllers/album.js
+++ browser/js/controllers/album.js
@@ -26,42 +26,16 @@ app.controller('AlbumCtrl', function($scope, $http, $rootScope, StatsFactory, Pl
           PlayerFactory.resume();
       }
       else {
-          PlayerFactory.start(song)
+          PlayerFactory.start(song); 
       }
     }
-    $scope.playing = PlayerFactory.isPlaying();
-    $scope.currentSong = PlayerFactory.getCurrentSong();
+
+    $scope.playing = PlayerFactory.isPlaying;
+    $scope.currentSong = PlayerFactory.getCurrentSong;
   };
 
   $scope.playing = PlayerFactory.isPlaying;
   $scope.currentSong = PlayerFactory.getCurrentSong;
 
-  // // incoming events (from Player, toggle, or skip)
-  // $scope.$on('pause', pause);
-  // $scope.$on('play', play);
-  // $scope.$on('next', next);
-  // $scope.$on('prev', prev);
-
-  // // functionality
-  // function pause () {
-  //   $scope.playing = false;
-  // }
-  // function play (event, song){
-  //   $scope.playing = true;
-  //   $scope.currentSong = song;
-  // };
-
-  // // a "true" modulo that wraps negative to the top of the range
-  // function mod (num, m) { return ((num%m)+m)%m; };
-
-  // // jump `val` spots in album (negative to go back)
-  // function skip (val) {
-  //   if (!$scope.currentSong) return;
-  //   var idx = $scope.album.songs.indexOf($scope.currentSong);
-  //   idx = mod( (idx + (val || 1)), $scope.album.songs.length );
-  //   $rootScope.$broadcast('play', $scope.album.songs[idx]);
-  // };
-  // function next () { skip(1); };
-  // function prev () { skip(-1); };
 
 });
--- browser/js/controllers/player.js
+++ browser/js/controllers/player.js
@@ -1,39 +1,20 @@
 app.controller('PlayerCtrl', function($scope, $rootScope, PlayerFactory){
-
-    $scope.playing = PlayerFactory.playing;
-    //$scope.currentSong = 'hello';$rootScope.currentSong;
-   // console.log(PlayerFactory.currentSong);
-    console.log(this);
-    $scope.currentSong = PlayerFactory.getCurrentSong;
-    
-  //   $scope.currentSong = PlayerFactory;
-  //    $scope.toggle = function (song) {
-  //   if (PlayerFactory.isPlaying()) PlayerFactory.pause();
-  //   else {
-  //     if (song === PlayerFactory.getCurrentSong()) {
-  //         PlayerFactory.resume();
-  //     }
-  //     else {
-  //         PlayerFactory.start(song)
-  //     }
-  //   }
-  //   $scope.playing = PlayerFactory.playing;
-  //   $scope.currentSong = PlayerFactory.getCurrentSong();
-  //   console.log($scope.currentSong);
-  // }
+  // main toggle
   $scope.toggle = function (song) {
-    if (PlayerFactory.playing) PlayerFactory.pause();
+    if (PlayerFactory.isPlaying()) PlayerFactory.pause();
     else {
       if (song === PlayerFactory.getCurrentSong()) {
           PlayerFactory.resume();
       }
       else {
-          PlayerFactory.start(song)
+          PlayerFactory.start(song); 
       }
     }
-    console.log(PlayerFactory.currentSong);
-    $scope.playing = PlayerFactory.playing;
-    $scope.currentSong = PlayerFactory.getCurrentSong();
-  }
 
+    $scope.playing = PlayerFactory.isPlaying;
+    $scope.currentSong = PlayerFactory.getCurrentSong;
+  };
+
+  $scope.playing = PlayerFactory.isPlaying;
+  $scope.currentSong = PlayerFactory.getCurrentSong;
 });
